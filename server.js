9f3b0912920da420e98f5975e89e6d353a6ad9b9
Ryan Strickler <ryan.strickler.1993@gmail.com>
2017-10-01 15:37:04 -0400

    added comments

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000; //port initialized
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+//routes
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
+//port started
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
