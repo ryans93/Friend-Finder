00cc915642faed344532fac31a51fc1f82dfb486
Ryan Strickler <ryan.strickler.1993@gmail.com>
2017-10-01 15:38:16 -0400

    added comments

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,11 @@ var path = require("path");
 
 module.exports = function (app) {
 
-    app.get("/survey", function (req, res) {
+    app.get("/survey", function (req, res) { //return survey page
         res.sendFile(path.join(__dirname, "/../public/survey.html")); 
     });
 
-    app.use(function (req, res) {
+    app.use(function (req, res) { //return home page
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
 }
